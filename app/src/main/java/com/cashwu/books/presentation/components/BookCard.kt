0e708918de11d9f0b36253ae8ec73daf70bdaaa4
Cash <cashwugeek@icloud.com>
2025-03-04 17:55:54 +0800

    ch05 Refactor BookCard to use delegated property for title state management

--- app/src/main/java/com/cashwu/books/presentation/components/BookCard.kt
+++ app/src/main/java/com/cashwu/books/presentation/components/BookCard.kt
@@ -19,9 +19,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -59,19 +61,19 @@ fun BookCard(book: BookVM, onDeleteClick: (BookVM) -> Unit) {
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Column {
-                    val title : MutableState<String> = remember { mutableStateOf(book.title) }
+                    var title : String by remember { mutableStateOf(book.title) }
                     Text(
-                        title.value,
+                        title,
                         style = TextStyle(fontSize = 32.sp, color = book.bookType.foregroundColor),
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis
                     )
                     OutlinedTextField(
-                        value = title.value,
+                        value = title,
                         textStyle = TextStyle(fontSize = 32.sp, color = book.bookType.foregroundColor),
                         maxLines = 1,
                         onValueChange = {
-                            title.value = it
+                            title = it
                         }
                     )
                 }
