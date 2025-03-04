77c330ccb89b64e41134bce98b3b90aa8b5d1db4
Cash <cashwugeek@icloud.com>
2025-03-04 14:36:00 +0800

    ch04 Enhance BookCard and ListBookScreen with borders and layout adjustments

--- app/src/main/java/com/cashwu/books/presentation/ListBookScreen.kt
+++ app/src/main/java/com/cashwu/books/presentation/ListBookScreen.kt
@@ -1,13 +1,16 @@
 package com.cashwu.books.presentation
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.cashwu.books.presentation.components.BookCard
 
@@ -19,7 +22,11 @@ import com.cashwu.books.presentation.components.BookCard
  */
 @Composable
 fun ListBookScreen(innerPadding: PaddingValues) {
-    LazyColumn (modifier = Modifier.padding(innerPadding)) {
+    LazyColumn (modifier = Modifier
+        .padding(innerPadding)
+        .border(1.dp, color = Color.Blue)
+        .fillMaxSize()
+    ) {
         books.forEach { book ->
             item {
                 BookCard(book)
--- app/src/main/java/com/cashwu/books/presentation/components/BookCard.kt
+++ app/src/main/java/com/cashwu/books/presentation/components/BookCard.kt
@@ -1,8 +1,13 @@
 package com.cashwu.books.presentation.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.core.provider.FontsContractCompat.Columns
 import com.cashwu.books.presentation.BookVM
 
@@ -15,7 +20,10 @@ import com.cashwu.books.presentation.BookVM
 @Composable
 fun BookCard(book: BookVM) {
 
-    Column {
+    Column(modifier = Modifier
+        .border(width = 1.dp, color = Color.Red)
+        .fillMaxSize()
+    ) {
         Text(book.title)
         Text(book.author)
     }
