be9ba55f9dad42b74a7f588a19855b66fae46eb3
Cash <cashwugeek@icloud.com>
2025-03-10 16:24:43 +0800

    ch08 Update Room dependencies in build.gradle.kts and libs.versions.toml for consistency and improved integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.common)
-    implementation(libs.room.ktx)
+    annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
